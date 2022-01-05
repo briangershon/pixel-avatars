764acba4e30f955d9dc8072b4c27ab337af51356
briangershon <briangershon@users.noreply.github.com>
2022-01-05 04:40:54 +0000

    Code style fixes

--- web-server/server.js
+++ web-server/server.js
@@ -1,5 +1,5 @@
 const port = 3000
-const brianTest2='abc';
+const brianTest2 = 'abc'
 
 require('./src/app').listen(port, () => {
     console.log(`Pixel webserver ready at http://localhost:${port}`)
